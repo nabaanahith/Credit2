ddeeba5b3852c31b5dcade546aa284d0e51695fd
nabaanahith <nabaa.nahidh@gmail.com>
2019-12-12 14:54:23 +0300

    v1

---
+++ precache-manifest.2986431fa4ab6604ae9ccc7d91fd9e6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c64a411894ba55194e9f45e645edced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39bf05e11d5af7691618",
+    "url": "/static/css/main.4ca6c8af.chunk.css"
+  },
+  {
+    "revision": "67bf9472e3ef774207bf",
+    "url": "/static/js/2.abe31a33.chunk.js"
+  },
+  {
+    "revision": "39bf05e11d5af7691618",
+    "url": "/static/js/main.9cb618f8.chunk.js"
+  },
+  {
+    "revision": "a065c5cf02d018d2dc27",
+    "url": "/static/js/runtime-main.db9b0e5f.js"
+  }
+]);
